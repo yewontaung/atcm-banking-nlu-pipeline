ef70f2b279511ea2e0ece27d17bacb8232e6b211
coderAung <yewontaung798@gmail.com>
2026-08-13 21:39:23 +0630

    feat: Add model 01 evaluation

---
+++ banking_nlu/evaluations/models/model_01_evaluator.py
@@ -0,0 +1,72 @@
+import torch
+
+from seqeval.metrics import accuracy_score, classification_report, precision_score, recall_score, f1_score
+from banking_nlu.evaluations.matrices import make_json_serializable
+from banking_nlu.utils.schemas import EvaluationData, EvaluationResult, Model01LogitOutput, Model02EvaluationResult, TransformerTokenizedDataset, TruthResult
+
+
+class Model01Evaluator:
+
+    def __init__(
+            self, 
+            model, 
+            tokenizer,
+            intent_encoder,
+            entity_encoder,
+            mapper,
+            device):
+        self.model = model
+        self.tokenizer = tokenizer
+        self.intent_encoder = intent_encoder
+        self.entity_encoder = entity_encoder
+        self.mapper = mapper
+        self.device = device
+
+    def get_evaluation_data(self, tokenized:list[TransformerTokenizedDataset]) -> EvaluationData:
+        true_intents = []
+        true_entities = []
+
+        pred_intents = []
+        pred_entities = []
+
+        self.model.eval()
+
+        for item in tokenized:
+            true_intents.append(self.intent_encoder.decode(item.intent_labels))
+            true_entities.append(self.entity_encoder.decode(item.ner_labels))
+    
+            token = self.tokenizer(item.text, return_tensors="pt", return_offsets_mapping=True, truncation=True)
+            input_ids = token["input_ids"].to(self.device)
+            attention_mask = token["attention_mask"].to(self.device)
+            offset_mapping = token["offset_mapping"][0]
+            with torch.no_grad():
+                outputs:Model01LogitOutput = self.model(input_ids, attention_mask)
+                pred_intents.append(self.mapper.intent_mapper.decode(outputs.intent_logits, text=item.text, offset_mapping=offset_mapping))
+                pred_entities.append(self.mapper.entity_mapper.decode(outputs.entity_logits, text=item.text, offset_mapping=offset_mapping))
+
+        return EvaluationData(
+            intent_truth=TruthResult(grounded_truth=true_intents, predicted_truth=pred_intents),
+            entity_truth=TruthResult(grounded_truth=true_entities, predicted_truth=pred_entities)
+        )
+
+    def calculate_matrices(self, evaluation_data:EvaluationData) -> Model02EvaluationResult:
+
+        intent_truth = evaluation_data.intent_truth
+        intent_evaluation = self.calculate_evaluation_result(intent_truth.grounded_truth, intent_truth.predicted_truth)
+
+        entity_truth = evaluation_data.entity_truth
+        entity_evaluation = self.calculate_evaluation_result(entity_truth.grounded_truth, entity_truth.predicted_truth)
+
+        return Model02EvaluationResult(
+            intent_evaluation=intent_evaluation,
+            entity_evaluation=entity_evaluation
+        )
+
+    def calculate_evaluation_result(self, grounded_truth, pred_truth) -> EvaluationResult:
+        return EvaluationResult(
+            report=make_json_serializable(classification_report(grounded_truth, pred_truth, output_dict=True)),
+            accuracy=accuracy_score(grounded_truth, pred_truth),
+            precision=precision_score(grounded_truth, pred_truth),
+            recall=recall_score(grounded_truth, pred_truth),
+            f1=f1_score(grounded_truth, pred_truth)
+        )    
---
+++ evaluate/model_01.py
@@ -0,0 +1,61 @@
+import torch
+from transformers import AutoTokenizer
+
+from banking_nlu.dataprocessors.encoders.bio import BIOLabelEncoder
+from banking_nlu.dataprocessors.encoders.classification import IntentClassificationEncoder
+from banking_nlu.dataprocessors.postprocessors.logit_mappers.classification import ClassificationLogitMapper
+from banking_nlu.dataprocessors.preprocessors import DataPreProcessor
+from banking_nlu.dataprocessors.tokenizers import Model01TransformerModelTokenizationProcessor, TextTokenizer
+from banking_nlu.evaluations.models.model_01_evaluator import Model01Evaluator
+from banking_nlu.models.model_01_transformer_model.model import Model01BankingNLUTransformerModel
+from banking_nlu.utils import env
+from banking_nlu.utils.loader import load_saved_model
+
+
+def evaluate():
+
+    tokenizer = AutoTokenizer.from_pretrained("xlm-roberta-base")
+
+    intent_encoder = IntentClassificationEncoder.from_file(env.INTENT_META_FILE)
+    entity_encoder = BIOLabelEncoder.from_file(env.ENTITY_META_FILE)
+    DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+    model = Model01BankingNLUTransformerModel(
+        model_name="xlm-roberta-base",
+        intent_count=intent_encoder.no_of_lables,
+        entity_count=entity_encoder.no_of_labels,
+    )
+
+    saved_model = load_saved_model(model, env.SAVED_MODEL_PATH, DEVICE)
+
+
+    tokenization_processor = Model01TransformerModelTokenizationProcessor(
+        tokenizer=TextTokenizer(tokenizer),
+        intent_encoder=intent_encoder,
+        entity_encoder=entity_encoder,
+    )
+
+    mapper = ClassificationLogitMapper(
+        intent_encoder=intent_encoder,
+        entity_encoder=entity_encoder
+    )
+
+
+    evaluator = Model01Evaluator(
+        model=saved_model,
+        tokenizer=tokenizer,
+        intent_encoder=intent_encoder,
+        entity_encoder=entity_encoder,
+        mapper=mapper,
+        device=DEVICE,
+    )
+    preprocessor = DataPreProcessor()
+    processed = preprocessor.process_file(env.TESTING_FILE)
+    tokenized = [tokenization_processor.process(item) for item in processed]
+
+    evaluation_data = evaluator.get_evaluation_data(tokenized)
+
+    print(evaluation_data.model_dump_json(indent=2))
+
+if __name__ == "__main__":
+    evaluate()
